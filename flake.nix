a368bfae4cd9b17c987e28f28625d1b44007cd6c
max <64974579+m-agahi@users.noreply.github.com>
2026-06-05 21:00:38 +0200

    chore: bump version 5.46.4 -> 5.46.5 + CHANGELOG + MIGRATION_NOTES

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
 
         yadgar-pkg = python.pkgs.buildPythonApplication {
           pname = "yadgar";
-          version = "5.46.4";
+          version = "5.46.5";
           format = "pyproject";
 
           src = ./.;
