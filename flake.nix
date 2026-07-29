0806fe650fc6e9bd8021831ad59a539e6412aa31
m-agahi <64974579+m-agahi@users.noreply.github.com>
2026-07-29 17:54:26 +0200

    chore(release): v5.169.0 -- core minor bump for the 13-car train (backend stays 5.59.0)

--- flake.nix
+++ flake.nix
@@ -123,7 +123,7 @@
 
         yadgar-pkg = python.pkgs.buildPythonApplication {
           pname = "yadgar";
-          version = "5.168.0";
+          version = "5.169.0";
           format = "pyproject";
 
           src = ./.;
@@ -258,7 +258,7 @@
 
             coreVersion = lib.mkOption {
               type = lib.types.str;
-              default = "5.168.0";
+              default = "5.169.0";
               description = "Container image tag for the yadgar core service.";
             };
 
